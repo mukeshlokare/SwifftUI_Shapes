239c961746aa9a0bdf3433895c62ebe73364203a
aplyamukesh <mukeshl@diipl.com>
2021-06-20 15:24:37 +0530

    Initial commit

---
+++ SwiftUIShapeDemo/SwiftUIShapeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShapeDemoApp.swift
+//  SwiftUIShapeDemo
+//
+//  Created by abc on 20/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShapeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PathShapeView()
+        }
+    }
+}
